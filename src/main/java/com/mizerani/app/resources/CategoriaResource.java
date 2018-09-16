6094da4dba5d2bcf4c196854f11bdfa1f9d00b98
Phelipe Mizerani <phelipemizerani@gmail.com>
2018-09-16 12:02:47 -0300

    Entidades categoria e produto

---
+++ src/main/java/com/mizerani/app/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.mizerani.app.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mizerani.app.domain.Categoria;
+import com.mizerani.app.services.CategoriaService;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		
+		Categoria categoria = categoriaService.buscar(id);
+		return ResponseEntity.ok().body(categoria);
+		
+	}
+	
+}
